ae203e0ef604cb9bb5ca13c5183400e22ddda4d5
Carlos Fuentes <Carlos@DESKTOP-0CMKI0F>
2021-02-22 12:37:59 +0100

    Second Commit

--- CalculadoraED/src/Resta.java
+++ CalculadoraED/src/Resta.java
@@ -13,7 +13,7 @@
 public class Resta {
 
 	public static void main (String [] args) {
-		System.out.println("hola");
+		System.out.println("hola que tal");
 	}
 	/** 
 	 * <b>Método 1:</b>  Resta de dos números reales, tendrá 2 parámetros de entrada y uno de salida, que será la solución.
